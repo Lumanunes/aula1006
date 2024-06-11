a39080883b7b5b4d6dd1b328f045a88979f1aeff
Lumanunes <lumapnunes48@gmail.com>
2024-06-11 08:53:57 -0300

    - Commit inicial

---
+++ calculadora.py
@@ -0,0 +1,29 @@
+# Calculadora Simples
+
+while True:
+    
+    print("\n\t\t\t -- Calculadora Simples--")
+    
+    print("1. Soma")
+    print("2. Sair")
+    
+    op = int(input("\nOpção: "))
+    
+    if op == 1:
+        print("\n\t\t\t-- Soma -- \n")
+        
+        # Entrada
+        n1 = float (input("Informe N1: "))
+        n2 = float (input("Informe N2: "))
+        
+        # Processamento 
+        total = n1 + n2
+        
+        # Saída 
+        print ("\n\t\t{} + {} ={}\n".format(n1,n2,total))
+        
+    elif op == 2:
+        print("\n\nAbraço!!\n\n")
+        break
+    else:
+        print("\n\nOpção {} incorreta!\n\n".format(op))
